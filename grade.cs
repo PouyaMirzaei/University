a503ceae0ff1f5f0f6b7ec60914858c8495234e1
Pouya Mirzaei <125562043+PouyaMirzaei@users.noreply.github.com>
2023-07-09 09:09:02 +0330

    Add files via upload

---
+++ grade.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Project{
+    class test1{
+        public static void Main(){
+
+            Console.WriteLine("Enter a: ");
+             double a  = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter b: ");
+             double b  = Convert.ToDouble(Console.ReadLine());
+             
+            double  Result = Calc(a,b);
+            if (Result >= 10){
+                Console.WriteLine("Pass");
+            }else{
+                Console.WriteLine("Failed");
+            }
+        }
+        private static double Calc(double a,double b){
+          double Result = (a*0.35)+(b*0.65);
+          return Result;
+        
+        }
+    }
+}
