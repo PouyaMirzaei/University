553c14f98e8b167bf18f2ebcbf3364cbc74786d2
Pouya Mirzaei <125562043+PouyaMirzaei@users.noreply.github.com>
2023-07-09 09:15:17 +0330

    Add files via upload

---
+++ sqrt2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace test{
+    class program{
+
+    
+    public static void Main(){
+         Console.WriteLine("Enter x: ");
+    double x = Convert.ToInt32(Console.ReadLine());
+    
+
+    Calc(x);
+         
+    }
+    private static void Calc(double x ){
+        double y = Math.Sqrt(Math.Abs(x)+Math.Sqrt(Math.Abs(x)+Math.Sqrt(Math.Abs(x))));
+      Console.WriteLine("Resault = " + y);
+    }
+ }
+}
