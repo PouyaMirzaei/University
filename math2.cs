35e8b3029b1c599935fa5d87de851ee0cde6c5e0
Pouya Mirzaei <125562043+PouyaMirzaei@users.noreply.github.com>
2023-07-09 09:10:40 +0330

    Add files via upload

---
+++ math2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace test
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter x: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Calc(x);
+        } 
+        private static void  Calc(int x)
+        {
+          double Result = Math.Pow(x,2)+(2*x)-4;
+          Console.WriteLine("Result: "+Result);   }   }  }  
