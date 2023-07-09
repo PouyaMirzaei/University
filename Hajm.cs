b416c0ee78f33d59a74e891c10228d46911321f8
Pouya Mirzaei <125562043+PouyaMirzaei@users.noreply.github.com>
2023-07-09 09:16:35 +0330

    Add files via upload

---
+++ Hajm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace GCD_LCM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers:");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("GCD: " + GCD(num1, num2));
+            Console.WriteLine("LCM: " + LCM(num1, num2));
+        }
+
+        static int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        static int LCM(int a, int b)
+        {
+            return (a * b) / GCD(a, b);
+        }
+    }
+}
+
+
+
+
+
