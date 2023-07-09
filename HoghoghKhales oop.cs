ff21e7befa98984c33a566e1a532eb8793d85e03
Pouya Mirzaei <125562043+PouyaMirzaei@users.noreply.github.com>
2023-07-09 09:13:30 +0330

    Add files via upload

---
+++ HoghoghKhales oop.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace Project
+{
+    class Hoghogh{
+    private double khales;
+    private double bimeh;
+    private double maliat;
+    
+    public double Khales{
+        get {return khales;}
+        set{khales = value;}
+    }
+
+    public double Bimeh{
+        get {return bimeh;}
+        set{bimeh = value;}
+    }
+
+    public double Maliat{
+        get {return maliat;}
+        set{maliat = value;}
+    }
+    
+    public Hoghogh(){}
+    public Hoghogh(double khales , double Bimeh , double maliat){
+        khales = Khales;
+        bimeh = Bimeh;
+        maliat = Maliat;
+    }
+    public double Calc(){
+        return (khales  - bimeh )/ maliat;
+        
+    }
+}
+    class Projects{
+        public static void Main(){
+              Console.WriteLine("khales: ");
+            double khales = Convert.ToDouble(Console.ReadLine());
+
+              Console.WriteLine("bime: ");
+            double bimeh = Convert.ToDouble(Console.ReadLine());
+
+              Console.WriteLine("maliat: ");
+            double maliat = Convert.ToDouble(Console.ReadLine());
+
+            Hoghogh pool = new Hoghogh(khales,bimeh,maliat);
+
+                 double P = pool.Calc();
+                 Console.WriteLine("p: "+P);
+            }
+        }
+    }
