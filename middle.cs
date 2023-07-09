8c641ef166f5026670fb5898e7105972e7f3c94b
Pouya Mirzaei <125562043+PouyaMirzaei@users.noreply.github.com>
2023-07-09 09:11:09 +0330

    Add files via upload

---
+++ middle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace test{
+    class test1{
+        public static void Main(){
+        int[] array = new int[10];
+        for(int i=0 ; i< 10 ; i++ ){
+            Console.WriteLine("Element {0} = ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+
+        }
+        Calc (array);
+        }
+        private static void Calc(int [] array){
+         for(int i=1 ; i <= 8 ; i++){
+           array[i] = (array[i-1] + array[i] + array[i+1])/3;
+
+         }
+         for(int i=0 ; i<=9 ; i++){
+            Console.WriteLine("Result= "+ array[i]);
+         }
+
+        }
+    }
+}
